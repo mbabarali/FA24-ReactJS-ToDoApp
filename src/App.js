6ac8f52709b33098e23e69bd9a96411e5ce6dab0
Muhammad Babar Ali <144947128+mbabarali@users.noreply.github.com>
2024-08-07 11:27:18 +0500

    Using dispacher & reducer functions - useReducer()
    An other State Hook

--- src/App.js
+++ src/App.js
@@ -1,142 +1,148 @@
 import AddTask from "./components/AddTask";
 import ManageTasks from "./components/ManageTasks";
 
-import { useState } from "react";
+import { useReducer } from "react";
 
 import "./App.css";
 
 import { INITIAL_TASKS } from "./data/initialTasks";
 
-// Stateful Functional Component
-function App() {
-  const [taskList, setTaskList] = useState(INITIAL_TASKS);
+// Reducer function must be defined outside the component
+// (e.g. App) to avoid recreation of reducer function on
+// rerendering (e.g. App) of the component.
+const reducerTaskList = (latestState, action) => {
+  const { type, payload } = action;
 
-  function handleDone(id) {
-    // ************** A HIGHLY RECOMMENDED APPROACH **************
-    // ************ Update State Variable(s) immutably ***********
-    // --- Three-step procedure to update the state, immutably ---
-    setTaskList((currentListState) => {
-      console.log("currentListState: ", currentListState);
-      const taskInd = currentListState.findIndex((task) => task.id === id);
+  switch (type) {
+    case "ADD": {
+      console.log("latestState: ", latestState);
 
-      const updatedList = [...currentListState]; // [STEP-1] Create copy // Shallow copy
+      // [STEP-1] Create copy // Shallow copy
+      const newList = [...latestState];
+
+      // [STEP-2] Update desired value(s) in copy
+      newList.push({
+        id: 0,
+        title: "",
+        createDate: "00.00.0000",
+        done: false,
+        trash: false,
+      });
+      newList[newList.length - 1].id = 100 + newList.length;
+      newList[newList.length - 1].title = payload.newTitle;
+      newList[newList.length - 1].createDate = new Date().toLocaleDateString(
+        "de-DE"
+      );
+      newList[newList.length - 1].done = false;
+      newList[newList.length - 1].trash = false;
+
+      console.log("newList: ", newList);
+      console.log(
+        "In Callback [Add-Executed] ==> Task " +
+          newList[newList.length - 1].id +
+          " at " +
+          (newList.length - 1) +
+          " --> Added "
+      );
+
+      return newList; // Do not forget to return updated copy
+    }
+    // break;
+    case "DONE": {
+      console.log("latestState: ", latestState);
+      const taskInd = latestState.findIndex((task) => task.id === payload.id);
+
+      const updatedList = [...latestState]; // [STEP-1] Create copy // Shallow copy
       updatedList[taskInd].done = true; // [STEP-2] Update desired value(s) in copy
 
       console.log("updatedList: ", updatedList);
       console.log("taskInd: ", taskInd);
       console.log(
         "In Callback [Done-Executed] ==> Task " +
-          id +
+          payload.id +
           " at " +
           taskInd +
           " --> done: ",
-        currentListState[taskInd].done
+        latestState[taskInd].done
       );
 
       return updatedList; // Do not forget to return updated copy
-    }); // [STEP-3] Update state with copy
+    }
+    // break;
+    case "DELETE": {
+      console.log("latestState: ", latestState);
 
-    console.log("In Handler [Done-Scheduled] ==> Task " + id + " --> done ");
-  }
-
-  function handleDelete(id) {
-    // ************** A HIGHLY RECOMMENDED APPROACH **************
-    // ************ Update State Variable(s) immutably ***********
-    // --- Three-step procedure to update the state, immutably ---
-    setTaskList((currentListState) => {
-      console.log("currentListState: ", currentListState);
-
-      const taskInd = currentListState.findIndex((task) => task.id === id);
-      const updatedList = [...currentListState]; // [STEP-1] Create copy // Shallow copy
+      const taskInd = latestState.findIndex((task) => task.id === payload.id);
+      const updatedList = [...latestState]; // [STEP-1] Create copy // Shallow copy
       updatedList[taskInd].trash = true; // [STEP-2] Update desired value(s) in copy
 
       console.log("updatedList: ", updatedList);
       console.log("taskInd: ", taskInd);
       console.log(
         "In Callback [Trash-Executed] ==> Task " +
-          id +
+          payload.id +
           " at " +
           taskInd +
           " --> trash: ",
-        currentListState[taskInd].trash
+        latestState[taskInd].trash
       );
 
       return updatedList; // Do not forget to return updated copy
-    }); // [STEP-3] Update state with copy
-
-    console.log("In Handler [Trash-Scheduled] ==> Task " + id + " --> trash ");
-  }
-
-  function handleAdd(newTitle) {
-    console.log("Add triggered!");
-
-    // ************** A HIGHLY RECOMMENDED APPROACH **************
-    // ************ Update State Variable(s) immutably ***********
-    // --- Three-step procedure to update the state, immutably ---
-    if (newTitle) {
-      setTaskList((currentListState) => {
-        console.log("currentListState: ", currentListState);
-
-        // [STEP-1] Create copy // Shallow copy
-        const newList = [...currentListState];
-
-        // [STEP-2] Update desired value(s) in copy
-        newList.push({
-          id: 0,
-          title: "",
-          createDate: "00.00.0000",
-          done: false,
-          trash: false,
-        });
-        newList[newList.length - 1].id = 100 + newList.length;
-        newList[newList.length - 1].title = newTitle;
-        newList[newList.length - 1].createDate = new Date().toLocaleDateString(
-          "de-DE"
-        );
-        newList[newList.length - 1].done = false;
-        newList[newList.length - 1].trash = false;
-
-        console.log("newList: ", newList);
-        console.log(
-          "In Callback [Add-Executed] ==> Task " +
-            newList[newList.length - 1].id +
-            " at " +
-            (newList.length - 1) +
-            " --> Added "
-        );
-
-        return newList; // Do not forget to return updated copy
-      }); // [STEP-3] Update state with copy
-
-      console.log("In Handler [Add-Scheduled] ==> Task --> Added ");
     }
-  }
+    // break;
+    case "RESTORE": {
+      console.log("latestState: ", latestState);
+      const taskInd = latestState.findIndex((task) => task.id === payload.id);
 
-  function handleRestore(id) {
-    // ************** A HIGHLY RECOMMENDED APPROACH **************
-    // ************ Update State Variable(s) immutably ***********
-    // --- Three-step procedure to update the state, immutably ---
-    setTaskList((currentListState) => {
-      console.log("currentListState: ", currentListState);
-      const taskInd = currentListState.findIndex((task) => task.id === id);
-
-      const updatedList = [...currentListState]; // [STEP-1] Create copy // Shallow copy
+      const updatedList = [...latestState]; // [STEP-1] Create copy // Shallow copy
       updatedList[taskInd].trash = false; // [STEP-2] Update desired value(s) in copy
 
       console.log("updatedList: ", updatedList);
       console.log("taskInd: ", taskInd);
       console.log(
         "In Callback [Restore-Executed] ==> Task " +
-          id +
+          payload.id +
           " at " +
           taskInd +
           " --> trash: ",
-        currentListState[taskInd].trash
+        latestState[taskInd].trash
       );
 
       return updatedList; // Do not forget to return updated copy
-    }); // [STEP-3] Update state with copy
+    }
+    // break;
+    default:
+      return latestState; // Do not forget to return ---- No change
+  }
+};
 
+// Stateful Functional Component
+function App() {
+  const [taskList, dispatchTaskList] = useReducer(
+    reducerTaskList,
+    INITIAL_TASKS
+  );
+
+  function handleDone(id) {
+    dispatchTaskList({ type: "DONE", payload: { id } });
+    console.log("In Handler [Done-Scheduled] ==> Task " + id + " --> done ");
+  }
+
+  function handleDelete(id) {
+    dispatchTaskList({ type: "DELETE", payload: { id } });
+    console.log("In Handler [Trash-Scheduled] ==> Task " + id + " --> trash ");
+  }
+
+  function handleAdd(newTitle) {
+    console.log("Add triggered!");
+    if (newTitle) {
+      dispatchTaskList({ type: "ADD", payload: { newTitle } });
+      console.log("In Handler [Add-Scheduled] ==> Task --> Added ");
+    }
+  }
+
+  function handleRestore(id) {
+    dispatchTaskList({ type: "RESTORE", payload: { id } });
     console.log(
       "In Handler [Restore-Scheduled] ==> Task " + id + " --> trash "
     );
