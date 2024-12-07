3bf4a50fa712135b7dee1e8ca856a13e1226c274
Muhammad Babar Ali <144947128+mbabarali@users.noreply.github.com>
2024-12-07 11:48:47 +0500

    Synchronous Side effects, infinite loop of render, and [Do] useState() Hook
    Synchronous side effects and [Do] useStatet()/useReducer Hooks

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,11 +1,21 @@
 import { useContext, useState } from "react";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import TaskListContext from "../store/taskList-context";
 
 // Functional Component
 function Edit({ id }) {
   const ctx = useContext(TaskListContext);
-  const [taskToEdit, setTaskToEdit] = useState({});
+
+  // [BETTER SOLUTION with Conditional rendering mechanism]
+  //  - Conditional rendering with state for synchronous code
+  //  - Synchronous side effect (useState or useReducer)
+  // ---- [SOLUTION-PROBLEM-ILR] ------------------------------
+  //     *  Conditional rendering mechanism
+  //     *  useEffect Hook
+  // ---------------------------------------------------------
+  const task = getTaskById(id);
+  // Passing reference, disregards mutability
+  const [taskToEdit, setTaskToEdit] = useState(task); // Now we can call setTaskToEdit function in asynchronous code such as event handlers without creating infinite renders
 
   function getTaskById(id) {
     let foundElements = ctx.taskList.filter((obj) => {
@@ -20,7 +30,7 @@ function Edit({ id }) {
   // // task && setTaskToEdit(task); // Passing reference, disregards immutability
   // task && setTaskToEdit({ ...task }); // Regards immutability
 
-  // [(Not a) SOLUTION with useEffect for synchronous code]
+  // [BAD SOLUTION with useEffect for synchronous code]
   // ---- [SOLUTION-PROBLEM-ILR] ------------------------------
   //     *  Conditional rendering mechanism
   //     *  useEffect Hook
@@ -33,16 +43,15 @@ function Edit({ id }) {
    * [REACT.DEV] Avoid resetting state on prop change in an Effect
    * [REACT.DEV] Avoid: Adjusting state on prop change in an Effect
    */
-  // useEffect(effect function, dependencies);
-
-  useEffect(() => {
-    // Synchronous side effect
-    console.log("  useEffect(() => {})");
-    const task = getTaskById(id);
-    // task && setTaskToEdit(task); // Passing reference, disregards immutability
-    task && setTaskToEdit({ ...task }); // Regards immutability
-  }, []); // With empty dependency array [Single execution]
-  // }); // Without dependency array [Infinite loop of rendering]
+  // // useEffect(effect function, dependencies);
+  // useEffect(() => {
+  //   // Synchronous side effect
+  //   console.log("  useEffect(() => {})");
+  //   const task = getTaskById(id);
+  //   // task && setTaskToEdit(task); // Passing reference, disregards mutability
+  //   task && setTaskToEdit({ ...task }); // Regards mutability
+  // }, []); // With empty dependency array [Single execution]
+  // // }); // Without dependency array [Infinite loop of rendering]
 
   console.log("function Edit({ id })");
 
@@ -56,9 +65,9 @@ function Edit({ id }) {
       </div>
 
       <div>
-        {/* <strong style={{ color: "blue" }}>[Local Variable] </strong> */}
+        <strong style={{ color: "blue" }}>[Local Variable] </strong>
         {/* {task ? Object.values(task) : undefined} */}
-        {/* {task ? JSON.stringify(task) : undefined} */}
+        {task ? JSON.stringify(task) : undefined}
       </div>
     </>
   );
